b4dc135939f98e36b4fee62d79ad56ff8abe534b
Didier Donsez <didier.donsez@gmail.com>
2019-06-12 08:38:08 +0200

    Create config.js example

---
+++ siconia/config.js
@@ -0,0 +1,16 @@
+{
+	"name": "Device configuration for devices",
+	"devEuiFilters": {
+		"range":["4883C7DF30050000", "4883C7DF3005FFFF"],
+		},
+	"compatiblity": {
+		"vHardwareMin": "0203",
+		"vSoftwareMin": "0103",
+		},
+	"authTokens": ["4883C7DF300500004883C7DF300500004883C7DF300500004883C7DF30050000"],
+	"customer": "script",
+	"jsCode": ["MotionHumidityTempPressure.js"],
+	"authenticatorPath": ".",
+	"authenticatorClass": "FolderCipheredAuthenticator",
+	"authenticatorParams" : [ ".\\EndPoints\\authkeys\\" ]
+}

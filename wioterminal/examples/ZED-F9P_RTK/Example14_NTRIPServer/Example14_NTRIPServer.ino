c978aa3188c4bad957d205c56f439a084fe7c3d5
Didier DONSEZ <didier.donsez@gmail.com>
2022-09-25 10:37:15 +0200

    improvement

--- wioterminal/examples/ZED-F9P_RTK/Example14_NTRIPServer/Example14_NTRIPServer.ino
+++ wioterminal/examples/ZED-F9P_RTK/Example14_NTRIPServer/Example14_NTRIPServer.ino
@@ -34,9 +34,14 @@
     pin 3 - Red - VCC on all Grove Connectors
     pin 4 - Black - GND on all Grove Connectors
 
+CHANGES: Didier DONSEZ for Wio Terminal
+    
 */
 #define WIO_TERMINAL 1
 //#define CENTIPEDE 1
+
+
+//#define BUF_HEXDUMP 1
 
 #ifdef WIO_TERMINAL
 #include "rpcWiFi.h"
@@ -75,6 +80,49 @@ uint32_t serverBytesSent = 0; //Just a running total
 
 long lastReport_ms = 0; //Time of last report of bytes sent
 
+void WifiScan(){
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+
+      Serial.println("scan start");
+ 
+    // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+    Serial.println("scan done");
+    if (n == 0) {
+        Serial.println("no networks found");
+    } else {
+        Serial.print(n);
+        Serial.println(" networks found");
+        for (int i = 0; i < n; ++i) {
+            // Print SSID and RSSI for each network found
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+            delay(10);
+        }
+    }
+    Serial.println("");
+}
+
+
+void WifiConnect() {
+  Serial.print("Connecting to local WiFi ");
+  WiFi.begin((char*)ssid, (char*)password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.print("\nWiFi connected with IP: ");
+  Serial.println(WiFi.localIP());
+}
+
 void setup()
 {
   Serial.begin(115200); // You may need to increase this for high navigation rates!
@@ -96,15 +144,9 @@ void setup()
       ;
   }
 
-  Serial.print("Connecting to local WiFi ");
-  WiFi.begin((char*)ssid, (char*)password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
- 
-  Serial.print("\nWiFi connected with IP: ");
-  Serial.println(WiFi.localIP());
+
+  WifiScan();
+  WifiConnect();
 
   myGPS.setI2COutput(COM_TYPE_UBX | COM_TYPE_NMEA | COM_TYPE_RTCM3); //UBX+RTCM3 is not a valid option so we enable all three.
 
@@ -126,15 +168,19 @@ void setup()
     while (1);
   }
   else
+  {
     Serial.println(F("NMEA disabled"));
+  }
 
-  //Enable necessary RTCM sentences
-  response &= myGPS.enableRTCMmessage(UBX_RTCM_1005, COM_PORT_I2C, 1); //Enable message 1005 to output through UART2, message every second
-  response &= myGPS.enableRTCMmessage(UBX_RTCM_1074, COM_PORT_I2C, 1);
-  response &= myGPS.enableRTCMmessage(UBX_RTCM_1084, COM_PORT_I2C, 1);
-  response &= myGPS.enableRTCMmessage(UBX_RTCM_1094, COM_PORT_I2C, 1);
-  response &= myGPS.enableRTCMmessage(UBX_RTCM_1124, COM_PORT_I2C, 1);
-  response &= myGPS.enableRTCMmessage(UBX_RTCM_1230, COM_PORT_I2C, 10); //Enable message every 10 seconds
+  // Enable necessary RTCM sentences
+  // All types are listed here : https://www.use-snip.com/kb/knowledge-base/rtcm-3-message-list  
+  //response &= myGPS.enableRTCMmessage(UBX_RTCM_1004, COM_PORT_I2C, 1); // Enable message 1004 (Extended L1&L2 GPS RTK Observables) to output through I2C, message every second
+  response &= myGPS.enableRTCMmessage(UBX_RTCM_1005, COM_PORT_I2C, 1); // Enable message 1005 (Stationary RTK Reference Station ARP) to output through I2C, message every second
+  response &= myGPS.enableRTCMmessage(UBX_RTCM_1074, COM_PORT_I2C, 1); // Enable message 1074 (GPS MSM4) to output through I2C, message every second
+  response &= myGPS.enableRTCMmessage(UBX_RTCM_1084, COM_PORT_I2C, 1); // Enable message 1084 (GLONASS MSM4) to output through I2C, message every second
+  response &= myGPS.enableRTCMmessage(UBX_RTCM_1094, COM_PORT_I2C, 1); // Enable message 1094 (Galileo MSM4) to output through I2C, message every second
+  response &= myGPS.enableRTCMmessage(UBX_RTCM_1124, COM_PORT_I2C, 1); // Enable message 1124 (BeiDou MSM4) to output through I2C, message every second
+  response &= myGPS.enableRTCMmessage(UBX_RTCM_1230, COM_PORT_I2C, 10); // Enable message 1230 (GLONASS L1 and L2 Code-Phase Biases) to output through I2C, message every 10 seconds
 
   if (response == false)
   {
@@ -150,6 +196,7 @@ void setup()
   //Note: If you leave these coordinates in place and setup your antenna *not* at SparkFun, your receiver
   //will be very confused and fail to generate correction data because, well, you aren't at SparkFun...
   //See this tutorial on getting PPP coordinates: https://learn.sparkfun.com/tutorials/how-to-build-a-diy-gnss-reference-station/all
+  
   response &= myGPS.setStaticPosition(-128020830, -80, -471680384, -70, 408666581, 10); //With high precision 0.1mm parts
   if (response == false)
   {
@@ -271,7 +318,7 @@ void beginServing()
         if (millis() - lastReport_ms > 250)
         {
           lastReport_ms += 250;
-          Serial.printf("Total sent: %d\n", serverBytesSent);
+          Serial.printf("\nTotal sent: %d\n", serverBytesSent);
         }
       }
     }
@@ -287,6 +334,107 @@ void beginServing()
   while (Serial.available()) Serial.read(); //Flush any endlines or carriage returns
 }
 
+// RTCM3 Utilities
+
+static void hexdump(const uint8_t* buf, const size_t len, const size_t linelen) {
+  for(size_t i=0; i<len; i++){
+     if(i%linelen==0) {
+        Serial.println();
+     }
+     if (buf[i] < 0x10) Serial.print("0");
+     Serial.print(buf[i], HEX);
+     Serial.print(" ");
+  }
+  Serial.println();
+}    
+
+/* extract unsigned/signed bits ------------------------------------------------
+* extract unsigned/signed bits from byte data
+* args   : unsigned char *buff I byte data
+*          int    pos    I      bit position from start of data (bits)
+*          int    len    I      bit length (bits) (len<=32)
+* return : extracted unsigned/signed bits
+*-----------------------------------------------------------------------------*/
+static unsigned int getbitu(const unsigned char *buff, int pos, int len)
+{
+    unsigned int bits=0;
+    int i;
+    for (i=pos;i<pos+len;i++) bits=(bits<<1)+((buff[i/8]>>(7-i%8))&1u);
+    return bits;
+}
+static int getbits(const unsigned char *buff, int pos, int len)
+{
+    unsigned int bits=getbitu(buff,pos,len);
+    if (len<=0||32<=len||!(bits&(1u<<(len-1)))) return (int)bits;
+    return (int)(bits|(~0u<<len)); /* extend sign */
+}
+
+/*
+ Dump type of RTCM3 message
+ message 1004 (Extended L1&L2 GPS RTK Observables) 
+ message 1005 (Stationary RTK Reference Station ARP) 
+ message 1074 (GPS MSM4) 
+ message 1084 (GLONASS MSM4) 
+ message 1094 (Galileo MSM4) 
+ message 1124 (BeiDou MSM4) 
+ message 1230 (GLONASS L1 and L2 Code-Phase Biases)
+*/
+static void dump_rtcm3_type(const unsigned int type) {
+  switch(type){
+    case 1004:
+      Serial.print("Extended L1&L2 GPS RTK Observables");
+      break;
+    case 1005:
+      Serial.print("Stationary RTK Reference Station ARP");
+      break;
+    case 1074:
+      Serial.print("GPS MSM4");
+      break;
+    case 1084:
+      Serial.print("GLONASS MSM4");
+      break;
+    case 1094:
+      Serial.print("Galileo MSM4");
+      break;
+    case 1124:
+      Serial.print("BeiDou MSM4");
+      break;
+    case 1230:
+      Serial.print("GLONASS L1 and L2 Code-Phase Biases");
+      break;
+    default:
+      Serial.print("Unknown");
+      break;
+  }
+}
+
+static int dump_rtcm3(const uint8_t* buf, const size_t len)
+{
+    double tow;
+    int ret=0,week;
+    unsigned int type=getbitu(buf,24,12);
+
+    if(buf[0]!=0xD3) {
+      Serial.print("NOT a RTCM3 message: len=");
+      Serial.print(len);
+      ret=-1;
+    } else {
+      Serial.print("RTCM3 message: len=");
+      Serial.print(len);
+      Serial.print(" type=");
+      Serial.print(type);
+      Serial.print(" : ");
+      dump_rtcm3_type(type);
+      
+      ret=0;
+    }
+
+    return ret;
+}
+
+uint8_t buf_for_rtcm3[256 * 4];
+uint8_t buf_for_rtcm3_idx = 0;
+
 //This function gets called from the SparkFun u-blox Arduino Library.
 //As each RTCM byte comes in you can specify what to do with it
 //Useful for passing the RTCM correction data to a radio, Ntrip broadcaster, etc.
@@ -297,5 +445,37 @@ void SFE_UBLOX_GPS::processRTCM(uint8_t incoming)
     client.write(incoming); //Send this byte to socket
     serverBytesSent++;
     lastSentRTCM_ms = millis();
+
+    if (myGPS.rtcmFrameCounter == 1) {
+      if(buf_for_rtcm3_idx!=0){
+        Serial.println();
+        int ret = dump_rtcm3(buf_for_rtcm3, buf_for_rtcm3_idx);
+        if(ret==-1) {
+           Serial.println();
+        } else {
+           Serial.println();
+        } 
+#ifdef BUF_HEXDUMP
+        hexdump(rtcmData, rtcmCount, 32);
+#endif
+      }
+      buf_for_rtcm3_idx=0;
+    }
+    buf_for_rtcm3[buf_for_rtcm3_idx++]=incoming;
+
+/*
+    //Pretty-print the HEX values to Serial
+    if (myGPS.rtcmFrameCounter == 1) {
+      Serial.println("");
+    }
+    if ((myGPS.rtcmFrameCounter -1) % 16 == 0) {
+      Serial.println("");
+      //Serial.print( myGPS.rtcmFrameCounter );
+      //Serial.print(" -");
+    }
+    Serial.print(" ");
+    if (incoming < 0x10) Serial.print("0");
+    Serial.print(incoming, HEX);
+*/
   }
 }

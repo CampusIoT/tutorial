c2dbe067792deffd0b6b479b2f48ed1f77159793
Didier DONSEZ <didier.donsez@gmail.com>
2022-09-25 10:07:52 +0200

    add url to report

--- wioterminal/examples/ZED-F9P_RTK/RTK_Example14_NTRIPServer/secrets.h
+++ wioterminal/examples/ZED-F9P_RTK/RTK_Example14_NTRIPServer/secrets.h
@@ -1,15 +1,16 @@
 //Your WiFi credentials
 
-char* ssid = "MySSID";
-char* password =  "mySSIDpassword";
-
+//char* ssid = "MySSID";
+//char* password =  "mySSIDpassword";
 
 const char* mntpnt = "GrenobleTinyGS";
 #ifdef CENTIPEDE
 //Your CENTIPEDE mount point credentials
 const char* mntpnt_pw = "centipede";
 #else
+// SNIP NTRIP: RTK2go - Caster Status Report http://rtk2go.com:2101/SNIP::MOUNTPT?baseName=GrenobleTinyGS
+
 //Your RTK2GO mount point credentials
-const char* mntpnt_pw = "MyRTK2GOpassword";
+//const char* mntpnt_pw = "MyRTK2GOpassword";
 //const char* mntpnt_pw = "WEEK2225"; 
 #endif

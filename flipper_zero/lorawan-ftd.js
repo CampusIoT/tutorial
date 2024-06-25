b9d57f808488e04dbd5e63067c4fe25d8c71f99a
Didier DONSEZ <didier.donsez@gmail.com>
2024-06-25 20:06:16 +0200

    add adr

--- flipper_zero/lorawan-ftd.js
+++ flipper_zero/lorawan-ftd.js
@@ -21,6 +21,7 @@ Pinout: Grove cable on Flipper GPIO
 Set baudrate at 9600
 */
 
+let notify = require("notification");
 
 let APPKEY = "1234567890ABCDEF1234567890ABCDEF";
 let PORT = 10;
@@ -51,6 +52,23 @@ function send(cmdline, nb_line, timeout, sentence) {
     return receive(nb_line, timeout, sentence);
 }
 
+function dec_dr(){
+    serial.write("AT+DR", [0x0d, 0x0a]);
+    let l = receive(4, 1000, "+DR: DR");
+    if(l) {
+        let dr = l.at(7) - 48; // '0' is 0x30 or 48
+        if(dr>0) {
+            dr--;
+        }
+        serial.write("AT+DR="+to_string(dr), [0x0d, 0x0a]);
+        receive(4, 1000, "+DR: DR");
+        print("DR="+to_string(dr));
+        return dr;
+    }
+    return undefined;
+}
+
+
 print("-------------------"); // for debug
 print("Flipper LoRaWAN FTD"); // for debug
 print("-------------------"); // for debug
@@ -58,26 +76,26 @@ delay(2000); // one second for debug
 
 let line;
 
-line = send("AT+ID", 5, 1000, "DevEui");
+line = send("AT+ID", 5, 500, "DevEui");
 print(line);
 delay(1000); // one second for debug
 
-send("AT+MODE=LWOTAA", 3, 1000);
+send("AT+MODE=LWOTAA", 3, 500);
 delay(1000); // one second for debug
 
-send("AT+DR=5", 4, 1000);
+send("AT+DR=5", 4, 500);
 delay(1000); // one second for debug
 
-send("AT+POWER=14", 4, 1000); // Change LoRaWAN AT module TX power to 14 dBm
+send("AT+POWER=14", 4, 500); // Change LoRaWAN AT module TX power to 14 dBm
 delay(1000); // one second for debug
 
-send("AT+ADR=ON", 4, 1000);
+send("AT+ADR=ON", 4, 500);
 delay(1000); // one second for debug
 
-send("AT+CLASS=A", 4, 1000);
+send("AT+CLASS=A", 4, 500);
 delay(1000); // one second for debug
 
-send("AT+KEY=APPKEY,"+APPKEY, 2, 1000);
+send("AT+KEY=APPKEY,"+APPKEY, 2, 500);
 delay(1000); // one second for debug
 
 // loop on JOIN if OTAA join procedure failed
@@ -89,11 +107,14 @@ while(!joined) {
     }
     if(!joined) {
         print("Join failed: retry later!");
+        notify.error();
         // retry until joined
         // TODO increment TxPower and decrement DR.
+        dec_dr();
         delay(10000); // 10 sec for duty cycle
     } else {
         print("Network joined");
+        notify.success();
     }
 }
 
@@ -107,18 +128,21 @@ let cpt = 1;
 while(true) {
     print("Msg #"+to_string(cpt)); // for debug
 
-    let dr_line = send("AT+DR", 4, 1000, " DR");
+    let dr_line = send("AT+DR", 5, 1000, "+DR: DR");
     print(dr_line);
 
     let rssi = send("AT+CMSG=FTD_"+to_string(cpt), 10, 1000, "RSSI");
     // TODO check if the message has been succesfully sent
     // TODO print downlink message
     // TODO retry immediatly when lower DR and higher TxPower if unconfirmed
+    console.log(rssi);
     if(rssi) {
         // TODO parse RSSI and SNR and get DN FCnt
         print("DN Cnf "+rssi);
+        notify.success();
     } else {
         print("DN Cnf not received");
+        dec_dr();
     }
     cpt++;
 
